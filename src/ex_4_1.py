697d4d419a2814cc329ecaa4c9021554dd3adc61
VijayUddandam <165346765+VijayUddandam@users.noreply.github.com>
2024-04-27 20:22:15 -0700

    completed_ex_4_0

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -17,7 +17,20 @@ def num_shutdowns(logfile):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    with open(logfile, 'r') as file:
+        log_data = file.readlines()
+
+    shutdown_count = 0
+    found_initiated = False
+
+    for line in log_data:
+        if "Shutdown initiated" in line:
+            found_initiated = True
+        elif "Shutdown complete" in line and found_initiated:
+            shutdown_count += 1
+            found_initiated = False
+
+    return shutdown_count
 
 
 # >>>> The code below will call your function and print the results
